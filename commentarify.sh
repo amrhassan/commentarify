27654aab43478a98cbee5d7b6facc1aabe1cac22
Amr Hassan <amr.hassan@gmail.com>
2019-12-27 15:01:15 +0100

    Drop volume filter

--- commentarify.sh
+++ commentarify.sh
@@ -1,6 +1,6 @@
 #! env bash
 
-# Mux-in a commentary track to your favorite movie.
+# Mix-in a commentary track to your favorite movie.
 
 # The path to the movie file
 MOVIE=""
@@ -8,19 +8,18 @@ MOVIE=""
 # The path to the commentary audio file
 COMMENTARY_AUDIO=""
 
-# The path to the output movie file
-OUTPUT=""
-
-# The ID of the movie audio track to mix with the commentary track
-MOVIE_AUDIO_TRACK_ID="1"
-
 # The desired delay of the commentary track (in ms)
 COMMENTARY_DELAY="0"
 
 # The desired title of the commentary track
 COMMENTARY_TITLE="Commentary"
 
-MOVIE_AUDIO_GAIN="0.1"
+# The ID of the movie audio track to mix with the commentary track
+MOVIE_AUDIO_TRACK_ID="1"
+
+# The path to the output movie file
+OUTPUT="${STR%.*}.$COMMENTARY_TITLE.${STR##*.}"
+
 
 set -o nounset
 
@@ -29,8 +28,7 @@ ffmpeg \
 	-i "$COMMENTARY_AUDIO" \
 	-filter_complex "[0:$MOVIE_AUDIO_TRACK_ID]pan=stereo|FL=FC+0.30FL+0.30BL|FR=FC+0.30FR+0.30BR[a00];\
 					 [1:a:0]adelay=$COMMENTARY_DELAY|$COMMENTARY_DELAY[a10];\
-					 [a00]volume=$MOVIE_AUDIO_GAIN[a01];\
-					 [a01][a10]amix=inputs=2:duration=first[a2]" \
+					 [a00][a10]amix=inputs=2:duration=first:weights=0.1 0.9[a2]" \
 	-map "[a2]" \
 	-map "0:v" \
 	-map "0:a" \
