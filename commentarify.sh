68ab15a22c42fa5f133788b440b8ed3a29cc5eb7
Amr Hassan <amr.hassan@gmail.com>
2019-12-27 12:11:05 +0100

    Fix docs

--- commentarify.sh
+++ commentarify.sh
@@ -5,14 +5,14 @@
 # The path to the movie file
 MOVIE=""
 
-# The ID of the movie audio track to mix with the commentary track
-MOVIE_AUDIO_TRACK_ID=""
-
-# The ID of the movie audio track to mix with the commentary track
+# The path to the commentary audio file
 COMMENTARY_AUDIO=""
 
 # The path to the output movie file
 OUTPUT=""
+
+# The ID of the movie audio track to mix with the commentary track
+MOVIE_AUDIO_TRACK_ID="1"
 
 # The desired delay of the commentary track (in ms)
 COMMENTARY_DELAY="0"
@@ -20,6 +20,7 @@ COMMENTARY_DELAY="0"
 # The desired title of the commentary track
 COMMENTARY_TITLE="Commentary"
 
+MOVIE_AUDIO_GAIN="0.1"
 
 set -o nounset
 
@@ -28,7 +29,7 @@ ffmpeg \
 	-i "$COMMENTARY_AUDIO" \
 	-filter_complex "[0:$MOVIE_AUDIO_TRACK_ID]pan=stereo|FL=FC+0.30FL+0.30BL|FR=FC+0.30FR+0.30BR[a00];\
 					 [1:a:0]adelay=$COMMENTARY_DELAY|$COMMENTARY_DELAY[a10];\
-					 [a00]volume=0.1[a01];\
+					 [a00]volume=$MOVIE_AUDIO_GAIN[a01];\
 					 [a01][a10]amix=inputs=2:duration=first[a2]" \
 	-map "[a2]" \
 	-map "0:v" \
